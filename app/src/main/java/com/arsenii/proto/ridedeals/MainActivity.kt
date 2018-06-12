6160fb5dc693f4cae5e3753f3bfb2dc493607324
Your Name <you@example.com>
2018-06-12 19:40:10 +0300

    add on back pressed

--- app/src/main/java/com/arsenii/proto/ridedeals/MainActivity.kt
+++ app/src/main/java/com/arsenii/proto/ridedeals/MainActivity.kt
@@ -15,19 +15,38 @@ class MainActivity : AppCompatActivity() {
 
         if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ) {
 
-            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-            StillerApi.putConfig("context", this)
-            StillerApi.putConfig("web_app_wrapper", web_wrapper)
-            StillerApi.putConfig("web_app_url", "http://192.168.25.104:8080/")
-            StillerApi.putConfig("web_app_server", "stiller")
-            StillerApi.putConfig("web_app_client", "stiller")
-            StillerApi.putConfig("url_whitelist", listOf( "gstatic.com" ))
-
-            StillerEnabledPluginsList.startAll()
-            StillerApi.init()
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
         }
 
+        initStiller()
+    }
 
+    fun initStiller() {
+
+        StillerApi.putConfig("context", this)
+        StillerApi.putConfig("web_app_wrapper", web_wrapper)
+        StillerApi.putConfig("web_app_url", "http://192.168.25.104:8080/")
+        StillerApi.putConfig("web_app_server", "stiller")
+        StillerApi.putConfig("web_app_client", "stiller")
+        StillerApi.putConfig("url_whitelist", listOf( "gstatic.com" ))
+
+        StillerEnabledPluginsList.startAll()
+        StillerApi.init()
+    }
+
+    override fun onBackPressed() {
+        
+        if ( web_wrapper.canGoBack() ) {
+
+            web_wrapper.goBack()
+
+        } else {
+
+            super.onBackPressed()
+        }
     }
 
 }
